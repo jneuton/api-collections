eba87a62026bfb9673dd7a4ca10d64e19c8a73bf
jneuton <jneuton.empreendedorismo@gmail.com>
2021-08-20 20:15:24 -0300

    created map test class

---
+++ app/src/main/java/com/jneuton/api_collections/TesteMap.kt
@@ -0,0 +1,13 @@
+package com.jneuton.api_collections
+
+fun main(){
+    val pair: Pair<String, Double> = Pair("Joao", 1000.0)
+    val map1 = mapOf(pair)
+    map1.forEach{(k, v)-> println("Chave: $k - valor: $v")}
+
+    val map2 = mapOf(
+        "Pedro" to 2500.0,
+        "Maria" to 3000.0
+    )
+    map2.forEach{(k, v)-> println("Chave: $k - valor: $v")}
+}
